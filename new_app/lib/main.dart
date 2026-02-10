4b9d788598911294fac36f3c760a0568fe3d00bd
kayqu <kayquec158@gmail.com>
2026-02-10 16:56:42 -0300

    Add Windows runner files for high DPI support and console integration
    - Created `runner.exe.manifest` to define DPI awareness and compatibility settings for the application.
    - Implemented `utils.cpp` and `utils.h` to manage console creation and command line argument handling.
    - Developed `win32_window.cpp` and `win32_window.h` to manage a high DPI-aware Win32 window, including theme updates and message handling.

---
+++ new_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+       
+      home: Scaffold(
+        appBar: AppBar(title: Text('Lista de Tarefas', style: TextStyle(color: Colors.white)), backgroundColor: Colors.black),
+
+        body: ListView(
+         children: [
+          ListTile( leading: Icon(Icons.radio_button_unchecked), title: Text('Estudar Flutter'), trailing: Icon(Icons.delete)),
+          ListTile( leading: Icon(Icons.radio_button_unchecked), title: Text('Praticar Dart'), trailing: Icon(Icons.delete)),
+          ListTile( leading: Icon(Icons.radio_button_unchecked), title: Text('Criar um App'), trailing: Icon(Icons.delete)),
+         ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}, backgroundColor: Colors.black, child: Icon(Icons.add, color: Colors.white,)),
+      ),
+    );
+  }
+} 
